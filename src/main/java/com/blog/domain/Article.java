a76fb1b19939a8d7cd69d9f9ea3fe2625969e829
Bill <64129036+sun-flower-free@users.noreply.github.com>
2021-11-28 23:34:15 +0800

    更改成使用 lombok 控制 DiaplayInVo

--- src/main/java/com/blog/domain/Article.java
+++ src/main/java/com/blog/domain/Article.java
@@ -27,58 +27,6 @@ public class Article {
 
     private String content;
 
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title == null ? null : title.trim();
-    }
-
-    public String getKeywords() {
-        return keywords;
-    }
-
-    public void setKeywords(String keywords) {
-        this.keywords = keywords == null ? null : keywords.trim();
-    }
-
-    public String getDesci() {
-        return desci;
-    }
-
-    public void setDesci(String desci) {
-        this.desci = desci == null ? null : desci.trim();
-    }
-
-    public String getPic() {
-        return pic;
-    }
-
-    public void setPic(String pic) {
-        this.pic = pic == null ? null : pic.trim();
-    }
-
-    public Integer getClick() {
-        return click;
-    }
-
-    public void setClick(Integer click) {
-        this.click = click;
-    }
-
-    public Date getTime() {
-        return time;
-    }
-
     public String getLocalTime() {
         SimpleDateFormat df = new SimpleDateFormat("yyyy-M-dd HH:mm");//设置日期格式
         String date = df.format(time);
@@ -86,24 +34,4 @@ public class Article {
 
     }
 
-
-    public void setTime(Date time) {
-        this.time = time;
-    }
-
-    public Integer getCatalogId() {
-        return catalogId;
-    }
-
-    public void setCatalogId(Integer catalogId) {
-        this.catalogId = catalogId;
-    }
-
-    public String getContent() {
-        return content;
-    }
-
-    public void setContent(String content) {
-        this.content = content == null ? null : content.trim();
-    }
 }
